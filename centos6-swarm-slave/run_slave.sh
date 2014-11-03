42e6b918c3e2e46bef445c2f8c9ab5c39b8df6e2
Dan Tian <dantian@gmail.com>
2014-11-03 11:30:57 -0800

    run swarm client as jenkins in jenkins home dir

--- centos6-swarm-slave/run_slave.sh
+++ centos6-swarm-slave/run_slave.sh
@@ -5,14 +5,14 @@ BUILDS_HOME=/home/jenkins
 
 # Run the slave
 launch_slave() {
-pkill -f swarm-client-jar-with-dependencies.jar
-java -jar ${BUILDS_HOME}/swarm-client-jar-with-dependencies.jar -master $JENKINS_MASTER_URL
-echo Slave started as process $$
+   pkill -f swarm-client-jar-with-dependencies.jar
+   cd $BUILDS_HOME && java -jar ${BUILDS_HOME}/swarm-client-jar-with-dependencies.jar -master $JENKINS_MASTER_URL
+   echo Slave started as process $$
 }
 
 # Get the necessary application and run the slave
 main() {
-launch_slave
+   launch_slave
 }
 
 main
